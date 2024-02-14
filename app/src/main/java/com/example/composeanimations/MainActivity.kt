717acfb66161e5829553019e34c4804999650c19
Artem <artem.mamedov5375@gmail.com>
2024-02-14 15:29:02 +0200

    Animated Content

--- app/src/main/java/com/example/composeanimations/MainActivity.kt
+++ app/src/main/java/com/example/composeanimations/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composeanimations.ui.screen.AnimateContent
 import com.example.composeanimations.ui.screen.Test
 import com.example.composeanimations.ui.theme.ComposeAnimationsTheme
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Test()
+                    AnimateContent()
                 }
             }
         }
